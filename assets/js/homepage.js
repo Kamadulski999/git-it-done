73ac5e80c0cb47d303258361bb00397ff04abd54
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-06-07 13:11:21 -0500

    added search for user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,77 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var user = "Kamadulski999"
 
-var getUserRepos = function() {
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-          });
-        });
-    };
-getUserRepos();
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+      } else{
+        alert("Please enter a gitHub username");
+      }
+  console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent = searchTerm;
+
+for (var i = 0; i < repos.length; i++) {
+  var repoName = repos[i].owner.login +"/" + repos[i].name;
+
+  var repoEl = document.createElement("div");
+  repoEl.classList = "list-item flex-row justify-space-between align-center"
+
+  var titleEl = document.createElement("span");
+  titleEl.textContent = repoName;
+  repoEl.appendChild(titleEl);
+
+  var statusEl = document.createElement("span");
+  statusEl.classList = "flex-row align-center"
+
+  if (repos[i].open_issues_count > 0) {
+    statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    console.log(repos[i].open_issues_count)
+  } else {
+    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+  }
+
+  repoEl.appendChild(statusEl);
+
+
+  repoContainerEl.appendChild(repoEl);
+
+ };};
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  })
+  .catch(function(error){
+    alert("Unable to connect to GitHub!");
+  });
+  
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
+getUserRepos("lernantino");
